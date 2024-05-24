7bf4a8a60c6ebd0a9aee0a65ebe7a36185feeaa8
shintanr <91821725+shintanr@users.noreply.github.com>
2024-05-24 14:27:20 +0700

    add submission

---
+++ submission1-storyapp/app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/UserRepository.kt
@@ -0,0 +1,38 @@
+package com.dicoding.picodiploma.loginwithanimation.data
+
+import androidx.lifecycle.LiveData
+import com.dicoding.picodiploma.loginwithanimation.data.pref.UserModel
+import com.dicoding.picodiploma.loginwithanimation.data.pref.UserPreference
+import kotlinx.coroutines.flow.Flow
+
+class UserRepository private constructor(
+    private val userPreference: UserPreference
+) {
+
+    fun signUp(name: String, email: String, password: String): LiveData<ResultState<RegisterResponse>> = liveData {
+
+    }
+
+    suspend fun saveSession(user: UserModel) {
+        userPreference.saveSession(user)
+    }
+
+    fun getSession(): Flow<UserModel> {
+        return userPreference.getSession()
+    }
+
+    suspend fun logout() {
+        userPreference.logout()
+    }
+
+    companion object {
+        @Volatile
+        private var instance: UserRepository? = null
+        fun getInstance(
+            userPreference: UserPreference
+        ): UserRepository =
+            instance ?: synchronized(this) {
+                instance ?: UserRepository(userPreference)
+            }.also { instance = it }
+    }
+}
